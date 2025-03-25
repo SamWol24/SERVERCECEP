036da1f86f77de9deba94402581aadf1d3e39ef4
Samir Agudelo Curaca <samiragudelo21@gmail.com>
2025-03-24 23:18:37 -0500

    Primer commit - Subiendo proyecto

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const app = require("./app/app");
+app.get("/", (req, res) => {
+    res.send("¡Servidor funcionando correctamente!");
+});
 
 app.listen(app.get('port'), ()=>{
     console.log(`servidor corriendo en el puerto ${app.get('port')}`);
